da615c3b170419c0c9074cb95de120bfd83fcdef
Johan Franco-Campos <johan.francocampos@gmail.com>
2025-02-20 09:24:37 -0500

    added temp stack.h

---
+++ stacks/Stack.h
@@ -0,0 +1,24 @@
+#include "LinkedList.h"
+
+
+template <class T>
+struct Stack : public LinkedList<T>
+{
+    void push(T item) {
+        LinkedList<T>::insert_at_front(item);
+    }
+
+    T pop() {
+        return LinkedList<T>::remove_from_front();
+    }
+
+    bool empty() const {
+        return (LinkedList<T>::length() == 0);
+    }
+
+    T top() {
+       if (LinkedList<T>::head == nullptr)
+           throw runtime_error("Can't return top item of empty stack!");
+       return LinkedList<T>::head->cargo;
+    }
+};
