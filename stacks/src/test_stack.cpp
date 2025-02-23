ea6162dd933623c58cda7e5078b4fa527841cfbd
Johan Franco-Campos <johan.francocampos@gmail.com>
2025-02-23 08:19:34 -0500

    importing everything so build works

---
+++ stacks/src/test_stack.cpp
@@ -0,0 +1,22 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <string>
+#include "Stack.h"
+
+using namespace std;
+
+TEST_CASE("Test basic stack operations on stack of ints") {
+    Stack<int> stack;
+    stack.push(9);
+    CHECK(stack.top() == 9);
+    stack.push(11);
+    CHECK(stack.top() == 11);
+    stack.push(42);
+    CHECK(stack.top() == 42);
+    CHECK(stack.pop() == 42);
+    CHECK(stack.top() == 11);
+    CHECK(stack.empty() == false);
+    stack.pop();
+    CHECK(stack.pop() == 9);
+    CHECK(stack.empty() == true);
+}
