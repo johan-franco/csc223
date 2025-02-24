b440437a846486be114bc5bcad9d2241ca9ffe78
Johan Franco-Campos <johan.francocampos@gmail.com>
2025-02-24 09:01:01 -0500

    second exercise

--- stacks/src/test_stack.cpp
+++ stacks/src/test_stack.cpp
@@ -20,3 +20,20 @@ TEST_CASE("Test basic stack operations on stack of ints") {
     CHECK(stack.pop() == 9);
     CHECK(stack.empty() == true);
 }
+
+TEST_CASE("Test basic stack operations on stack of strings") {
+    Stack<string> stack;
+    stack.push("cheese");
+    CHECK(stack.top() == "cheese");
+    stack.push("anchovies");
+    CHECK(stack.top() == "anchovies");
+    stack.push("onions");
+    CHECK(stack.top() == "onions");
+    CHECK(stack.pop() == "onions");
+    CHECK(stack.top() == "anchovies");
+    CHECK(stack.empty() == false);
+    stack.pop();
+    CHECK(stack.pop() == "cheese");
+    CHECK(stack.empty() == true);
+}
+
