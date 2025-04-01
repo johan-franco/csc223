b2b49be3d3883bd64e650bf2381b26d4d8b8b871
Johan Franco-Campos <johan.francocampos@gmail.com>
2025-04-01 08:16:48 -0400

    added realization of error

--- ruby/Cards/Card.rb
+++ ruby/Cards/Card.rb
@@ -2,8 +2,10 @@ class Card #unlike C++ classes aren't automatically private so we can use classe
     attr_accessor :suit, :rank #to generate commands to grab info
 
     def initialize(s,r) #can;t have mult initializers
-        @@suit = s
-        @@rank = r
+        #apparently using @@ means that all cards have the same values across all instances
+        #previously i used it because i thought it would be necessary for the other classes like deck.
+        @suit = s
+        @rank = r
     end
 
     def to_string()
@@ -13,12 +15,12 @@ class Card #unlike C++ classes aren't automatically private so we can use classe
         @rank_strings = ["Joker", "Ace", "2", "3", "4", "5", "6",
         "7", "8", "9", "10", "Jack", "Queen", "King"]
         #if rank == 0 then return joker else return to_string ver of card
-        return @@rank == 0 ? @rank_strings[@@rank] : @rank_strings[@@rank] + " of " + @suit_strings[@@suit]
+        return @rank == 0 ? @rank_strings[@rank] : @rank_strings[@rank] + " of " + @suit_strings[@suit]
     end
 
     def ==(other_card)
         #the more simple way I kept trying to do wasn't working but this does
-        if @@rank != other_card.rank  or @@suit != other_card.suit
+        if @rank != other_card.rank  or @suit != other_card.suit
             return false
         end
         return true
