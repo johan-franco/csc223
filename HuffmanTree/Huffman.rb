b7126eed3943f59da408d134eaf738e96f084ef5
Johan Franco-Campos <johan.francocampos@gmail.com>
2025-05-04 18:11:02 -0400

    finished starting build tree method and added potential insert method

--- HuffmanTree/Huffman.rb
+++ HuffmanTree/Huffman.rb
@@ -75,20 +75,40 @@ class HuffInternalNode
 end
 
 class HuffTree
-    #currently can't have root node as leaf
-    @root = HuffInternalNode.new(nil)
+
+    def initialize(frequency)
+        @leaf_nodes = frequency
+        @root = build_tree(@leaf_nodes)
+    end
+    
 
     def build_tree(chawei)
         #transform all of input array elements into leaf nodes
-        leaf_nodes = chawei.map { |char, weight| HuffLeafNode.new(char,weight) }
+        nodes = chawei.map { |char, weight| HuffLeafNode.new(char,weight) }
         
-        #sort array of leaf nodes by weight to build from the ground up ()
-        nodes = leaf_nodes.sort_by { |leafnode| leafnode.weight }
 
         while nodes.size != 1
-            #need to take 2 elements from nodes out to create an internal node 
-            #then add internal node back into tree
-            #this should loop until only root element is left
+            #sort array of leaf nodes by weight to build from the ground up ()
+            nodes = nodes.sort_by { |node| node.weight }
 
-    #need build_tree method. need to take in in array and start building from ground up
+            #take the first element in array and assign to left as it should have the lowest weight
+            left = nodes.shift
+            right = nodes.shift
+
+            internal_weight = left.weight + right.weight
+
+            internal = HuffInternalNode.new(internal_weight, left,right)
+            
+            #put internal node back into array, this will loop until only root element is left
+            nodes << internal
+        end
+
+        return nodes.first
+    end
+
+    def insert(leaf_node)
+        @leaf_nodes << leaf_node
+        build_tree(@leaf_nodes)
+    end
+
 end
