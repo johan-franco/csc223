4729dc19c44df48f47ea102f44d2d9843a59b153
Johan Franco-Campos <johan.francocampos@gmail.com>
2025-05-04 18:30:57 -0400

    shortened code, removed pretty print and added some tests

--- HuffmanTree/Huffman.rb
+++ HuffmanTree/Huffman.rb
@@ -1,4 +1,5 @@
 class HuffLeafNode
+    attr_accessor :char, :weight
     def initialize(char, weight = nil)
         @char = char
         @weight = weight
@@ -7,28 +8,12 @@ class HuffLeafNode
     def isLeaf
         return true
     end
-    #getter functions
-    def char
-        @char
-    end
-  
-    def weight
-        @weight
-    end
 
-  #setter functions
-    def set_weight(new_weight)
-        @weight = new_weight
-    end
-  
-    def set_char(new_char)
-        @char = new_char
-    end
-
-  
 end
 
 class HuffInternalNode
+    attr_accessor :left, :right, :weight
+
     def initialize(weight, left = nil, right = nil)
         @weight = weight
         @left = left 
@@ -38,43 +23,11 @@ class HuffInternalNode
     def isLeaf
         return false
     end
-  
-    #getter functions
-    def weight
-      @weight
-    end
-  
-    def left
-      @left
-    end
-  
-    def right
-      @right
-    end
 
-  #setter functions
-    def set_left(new_left)
-      @left = new_left
-    end
-  
-    def set_right(new_right)
-      @right = new_right
-    end
-  
-    def set_weight(new_weight)
-      @weight = new_weight
-    end
-  
-    #
-    def pretty_print(node = self, prefix = '', is_left = true)
-      pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-      puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data ? node.data : "nil"}"
-      pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
-    end
-  #
 end
 
 class HuffTree
+    attr_reader :root
 
     def initialize(frequency)
         @leaf_nodes = frequency
@@ -108,7 +61,33 @@ class HuffTree
 
     def insert(leaf_node)
         @leaf_nodes << leaf_node
-        build_tree(@leaf_nodes)
+        @root = build_tree(@leaf_nodes)
     end
 
 end
+
+test_frequencies = [
+  ['a', 5],
+  ['b', 9],
+  ['c', 12],
+  ['d', 13],
+  ['e', 16],
+  ['f', 45]         
+]
+
+#need to work on pretty print
+tree = HuffTree.new(test_frequencies)
+puts "\nTree structure:"
+#tree.root.pretty_print
+
+# testing root weight
+expected_root_weight = test_frequencies.sum { |char, weight| weight }
+actual_root_weight = tree.root.weight
+puts "\nVerification:"
+puts "Expected root weight: #{expected_root_weight}"
+puts "Actual root weight:   #{actual_root_weight}"
+
+# testing insert
+tree.insert(['g', 8])
+puts "\nUpdated tree structure:"
+tree.root.pretty_print
