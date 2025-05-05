72247538dd7cbe9ec7bcbcb2964529e1f0659322
Johan Franco-Campos <johan.francocampos@gmail.com>
2025-05-05 01:37:07 -0400

    created encode and decode and added visual using mermaid

--- HuffmanTree/Huffman.rb
+++ HuffmanTree/Huffman.rb
@@ -9,6 +9,7 @@ class HuffLeafNode
         return true
     end
 
+
 end
 
 class HuffInternalNode
@@ -32,6 +33,8 @@ class HuffTree
     def initialize(frequency)
         @leaf_nodes = frequency
         @root = build_tree(@leaf_nodes)
+        @code = {}
+
     end
     
 
@@ -64,21 +67,68 @@ class HuffTree
         @root = build_tree(@leaf_nodes)
     end
 
+    def decode(input)
+        trav = @root
+        message = ""
+        input.each_char do |bit|
+            if bit == '0'
+                trav = trav.left
+            else
+                trav = trav.right
+            end
+
+            if trav.isLeaf == true
+                message << trav.char
+                trav = @root
+            end
+        end
+        return message
+    end
+
+    def encode(text)
+        codes = generate_codes
+        
+        encoded = text.chars.map do |char|
+            codes[char] 
+        end.join
+        
+        return encoded
+    end
+    
+    def generate_codes
+        codes = {}
+        # Start traversal from root with empty path
+        traverse_for_codes(@root, "", codes)
+        codes
+    end
+    
+    def traverse_for_codes(node, current_path, codes)
+        if node.isLeaf
+          # Record the character's code when we reach a leaf
+            codes[node.char] = current_path unless node.char.nil?
+        else
+          # Recursively traverse left (0) and right (1)
+            traverse_for_codes(node.left, current_path + "0", codes)
+            traverse_for_codes(node.right, current_path + "1", codes)
+        end
+    end
+
+
+
 end
 
 test_frequencies = [
-  ['a', 5],
-  ['b', 9],
-  ['c', 12],
-  ['d', 13],
-  ['e', 16],
-  ['f', 45]         
+    ['a', 5],
+    ['b', 9],
+    ['c', 12],
+    ['d', 13],
+    ['e', 16],
+    ['f', 45]
 ]
 
 #need to work on pretty print
 tree = HuffTree.new(test_frequencies)
 puts "\nTree structure:"
-#tree.root.pretty_print
 
 # testing root weight
 expected_root_weight = test_frequencies.sum { |char, weight| weight }
@@ -90,4 +140,17 @@ puts "Actual root weight:   #{actual_root_weight}"
 # testing insert
 tree.insert(['g', 8])
 puts "\nUpdated tree structure:"
-tree.root.pretty_print
+
+test_string = "abcdef"
+encoded = tree.encode(test_string)
+decoded = tree.decode(encoded)
+puts "\nTest 1: Roundtrip '#{test_string}'"
+puts "Encoded: #{encoded}"
+puts "Decoded: #{decoded}"
+
+# Test 2: Single character
+test_char = "f"
+encoded = tree.encode(test_char)
+puts "\nTest 2: Single character '#{test_char}'"
+puts "Encoded: #{encoded}"  
+
