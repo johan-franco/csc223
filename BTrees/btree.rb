00390d5ce565e7618496a949364e9d65211145d7
Johan Franco-Campos <johan.francocampos@gmail.com>
2025-05-13 09:17:21 -0400

    added more to code

--- BTrees/btree.rb
+++ BTrees/btree.rb
@@ -1,19 +1,48 @@
 class RootNode
-    def initialize
+    attr_accessor :maxval, :values, :paths
+    def initialize()
         @maxval = 4
         @values = []
-        @paths = {}
+        @paths = []
     end
 
     def isRoot
         return true
     end
 
+    def insert(val)
+        if @values.length() == @maxval
+            @values.insert(val)
+            @values.sort!
+            if paths.empty
+                paths[0] = Node.new(@values.slice(0,1))
+                paths[1] = Node.new(@values.slice(3,4))
+
+
+            else
+                #do something else
+                return
+            end
+            return
+        else 
+            @values.insert(val)
+            @values.sort!
+            update_paths()
+        end
+    
+    end
+
+    def update_paths
+        return
+    end
+
 end
 
 class Node < RootNode
-    def initialize
+    attr_accessor :minval
+    def initialize(val = nil)
         super
+        @values = val
         @minval = @maxval/2
     end
 
@@ -24,8 +53,20 @@ class Node < RootNode
 end
 
 class BTree
+    attr_reader :root
     def initialize
-        @root
+        @root = RootNode.new()
     end
 
+    def insert(val)
+        traverse(val).insert(val)
+    end
+    
+    def traverse(val)
+        if @roots.path.empty
+            return @root
+        else
+            #traverse using val
+        end
+    end
 end
